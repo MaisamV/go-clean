51c045f8eccd5980e1822f314743ad9b0774a0e0
MaisamV <maisam.vahidsafa@gmail.com>
2022-07-26 13:42:55 +0430

    Fixed HealthRepo connection release issue

--- PgxRepo/healthRepo.go
+++ PgxRepo/healthRepo.go
@@ -4,20 +4,20 @@ import (
 	"GoCleanMicroservice/Common"
 	"GoCleanMicroservice/Domain/Model"
 	"context"
-	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type HealthRepo struct {
-	pool *pgxpool.Pool
+	Pool *pgxpool.Pool
 }
 
 func (r *HealthRepo) Check() Model.HealthResponse {
-	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	var query pgx.Rows
 	var err error
 	var elapsedTime = Common.MeasureTime(func() {
-		query, err = r.pool.Query(context.Background(), "select 1")
+		var query, e = r.Pool.Query(context.Background(), "select 1;")
+		err = e
+		for query.Next() {
+		}
 	})
 	return Model.HealthResponse{
 		IsConnected: err == nil,
