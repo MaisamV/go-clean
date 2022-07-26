15ff3256f164734264cc38ee810f2dd42a1307c6
MaisamV <maisam.vahidsafa@gmail.com>
2022-07-26 13:46:01 +0430

    Used InteractorPackage instead of passing Interactors one by one

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"GoCleanMicroservice/Delivery"
 	"GoCleanMicroservice/Domain"
 	"GoCleanMicroservice/GinDelivery"
+	"GoCleanMicroservice/PgxRepo"
+	"GoCleanMicroservice/Repo"
 )
 
 func main() {
-	var pingInteractor = createInteractor()
-	var server = createServer(&pingInteractor)
+	var pingInteractor = createPingInteractor()
+	var healthRepo = createHealthRepo()
+	var healthInteractor = createHealthInteractor(&healthRepo)
+	var interactors = createInteractorPackage(&pingInteractor, &healthInteractor)
+	var server = createServer(interactors)
 	err := server.Init()
 	if err != nil {
 		return
@@ -20,10 +25,23 @@ func main() {
 	}
 }
 
-func createInteractor() Domain.PingInteractor {
+func createPingInteractor() Domain.PingInteractor {
 	return &Core.PingInteractor{}
 }
 
-func createServer(i *Domain.PingInteractor) Delivery.Server {
-	return &GinDelivery.Server{Interactors: &GinDelivery.InteractorPackage{Interactor: i}}
+func createHealthInteractor(healthRepo *Repo.HealthRepo) Domain.HealthInteractor {
+	return &Core.HealthInteractor{HealthRepo: healthRepo}
+}
+
+func createHealthRepo() Repo.HealthRepo {
+	pool, _ := (&PgxRepo.PoolFactory{}).Create()
+	return &PgxRepo.HealthRepo{Pool: pool}
+}
+
+func createServer(i *GinDelivery.InteractorPackage) Delivery.Server {
+	return &GinDelivery.Server{Interactors: i}
+}
+
+func createInteractorPackage(p *Domain.PingInteractor, h *Domain.HealthInteractor) *GinDelivery.InteractorPackage {
+	return &GinDelivery.InteractorPackage{Interactor: p, Health: h}
 }
