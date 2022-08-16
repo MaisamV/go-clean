3d3f9c02c57003394aebe073256c15ac3a67a35d
MaisamV <maisam.vahidsafa@gmail.com>
2022-08-16 17:40:33 +0430

    Implemented ping grpc service

--- GrpcGoDelivery/services.go
+++ GrpcGoDelivery/services.go
@@ -1,7 +1,14 @@
 package GrpcGoDelivery
 
+import (
+	"GoCleanMicroservice/GrpcGoDelivery/service"
+	pb "GoCleanMicroservice/GrpcGoDelivery/service"
+)
+
 func (m *Server) setInteractors() {
+	service.InitPingInteractor(m.Interactors.Interactor)
 }
 
 func (m *Server) addServices() {
+	pb.RegisterPingerServer(m.Engine, &service.PingServer{})
 }
