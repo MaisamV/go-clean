94c88bfb25a4ca10cc7cb6ab8c498eb8add01391
MaisamV <maisam.vahidsafa@gmail.com>
2022-08-16 17:33:53 +0430

    Added postgres database connection string as example

--- PgxRepo/poolFactory.go
+++ PgxRepo/poolFactory.go
@@ -10,5 +10,6 @@ type PoolFactory struct {
 }
 
 func (p *PoolFactory) Create() (*pgxpool.Pool, error) {
+	// urlExample := "postgres://username:password@localhost:5432/database_name?pool_min_conns=10&pool_max_conns=100"
 	return pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 }
