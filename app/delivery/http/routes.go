8b18e02551a2ea6314b39d1b244a798684172e15
MaisamV <maisam.vahidsafa@gmail.com>
2022-08-17 18:51:27 +0430

    Added interactor validation to http module

--- app/delivery/http/routes.go
+++ app/delivery/http/routes.go
@@ -5,9 +5,17 @@ import (
 )
 
 func (m *Server) addRoutes() {
+	pingInteractor := m.Interactors.Interactor
+	if pingInteractor == nil {
+		panic(any("Ping interactor cannot be null."))
+	}
 	m.Engine.GET("/ping", route.Ping)
 	route.InitPingInteractor(m.Interactors.Interactor)
 
+	healthInteractor := m.Interactors.Health
+	if healthInteractor == nil {
+		panic(any("Health interactor cannot be null."))
+	}
 	m.Engine.GET("/health", route.Health)
 	route.InitHealthInteractor(m.Interactors.Health)
 }
