b8ff71a3f737fed4b784f440207d2d9a6c443b09
MaisamV <maisam.vahidsafa@gmail.com>
2022-08-17 17:23:08 +0430

    Fixed pool factory

--- app/repo/pgx/pool_factory.go
+++ app/repo/pgx/pool_factory.go
@@ -3,6 +3,7 @@ package pgx
 import (
 	"context"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"os"
 )
 
 type PoolFactory struct {
@@ -10,5 +11,5 @@ type PoolFactory struct {
 
 func (p *PoolFactory) Create() (*pgxpool.Pool, error) {
 	// urlExample := "postgres://username:password@localhost:5432/database_name?pool_min_conns=10&pool_max_conns=100"
-	return pgxpool.Connect(context.Background(), "postgres://postgres:12345trewq@192.168.127.139:5432/postgres?pool_min_conns=10&pool_max_conns=100")
+	return pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 }
